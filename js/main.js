ba03ee5d8a10150e4e392c7d7b427eae14538e4b
mostafawaleed3 <mostafawaleed206@gmail.com>
2022-06-30 12:14:49 +0200

    improvements

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 /*==================== SHOW MENU ====================*/
-const navMenu = document.getElementById('js-nav-menu'),
-	navToggle = document.getElementById('js-nav-toggle'),
-	navClose = document.getElementById('js-nav-close');
-	
-navToggle.addEventListener('click', () => {
-	navMenu.setAttribute('data-visible', true);
+const navMenu = document.getElementById("js-nav-menu"),
+  navToggle = document.getElementById("js-nav-toggle"),
+  navClose = document.getElementById("js-nav-close");
+
+navToggle.addEventListener("click", () => {
+  navMenu.setAttribute("data-visible", true);
 });
 
 // ===== MENU CLOSE =====
-navClose.addEventListener('click', () => {
-	navMenu.setAttribute('data-visible', false);
+navClose.addEventListener("click", () => {
+  navMenu.setAttribute("data-visible", false);
 });
